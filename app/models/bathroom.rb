fc65e235826d3c9de439b5343f49904c8e96231b
Robin Weidlich <robinweidlich7@gmail.com>
2020-03-05 15:15:42 -0300

    add geocoded_by and after_validation to model + inserted mapbox api_key

--- app/models/bathroom.rb
+++ app/models/bathroom.rb
@@ -1,4 +1,6 @@
 class Bathroom < ApplicationRecord
   has_many :bathroom_tags
   has_many :tags, through: :bathroom_tags
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
