dd5ee89b339769e57f96d81c83fc68f693191a3a
Robin Weidlich <robinweidlich7@gmail.com>
2020-03-05 16:57:33 -0300

    create markers and zoom in

--- app/controllers/bathrooms_controller.rb
+++ app/controllers/bathrooms_controller.rb
@@ -2,6 +2,13 @@ class BathroomsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+  	@bathrooms = Bathroom.geocoded #returns flats with coordinates
+    @markers = @bathrooms.map do |bathroom|
+      {
+        lat: bathroom.latitude,
+        lng: bathroom.longitude
+      }
+    end
     @bathrooms = Bathroom.all
   end
 end
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -14,7 +14,10 @@ const initMapbox = () => {
     		new mapboxgl.Marker()
       		.setLngLat([ marker.lng, marker.lat ])
       		.addTo(map);
-  });
+    });
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 500 });
   }
 };
 
