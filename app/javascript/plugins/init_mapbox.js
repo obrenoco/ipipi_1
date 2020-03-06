dc9ea0598df86b7f34be2bbf12353d93498e4ae7
breno romeiro <bsromeiro@gmail.com>
2020-03-06 15:03:49 -0300

    Remove Radius from current position

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,31 +5,41 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    // MAP STYLE
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+    //
+        // ADD MARKERS
+    const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // added this
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup) // added this
+          .addTo(map);
+    });
+    // ZOOM
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+    //
+    // GEOLOCATE
     map.addControl(new mapboxgl.GeolocateControl({
       positionOptions: {
           enableHighAccuracy: true
       },
       trackUserLocation: true
     }), 'bottom-right');
-    const markers = JSON.parse(mapElement.dataset.markers);
-  		markers.forEach((marker) => {
-        const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // added this
-    		new mapboxgl.Marker()
-      		.setLngLat([ marker.lng, marker.lat ])
-          .setPopup(popup) // added this
-      		.addTo(map);
-    });
+    //
 
+      //
+      // CURRENT POSITION
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 500 });
     navigator.geolocation.getCurrentPosition(function(position){});
-  }
-};
+    }
+  };
+      //
 
 export { initMapbox };
