c2668150d117121cbc123fc421d3a696bfc660a8
breno romeiro <bsromeiro@gmail.com>
2020-03-06 17:39:28 -0300

    add search bar + filter by region

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+// import MapboxGeocoder from @mapbox
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -23,20 +24,31 @@ const initMapbox = () => {
     // ZOOM
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
     //
+
     // GEOLOCATE
     map.addControl(new mapboxgl.GeolocateControl({
       positionOptions: {
-          enableHighAccuracy: true, zoom: 22
+          enableHighAccuracy: true, minZoom: 22
       },
       trackUserLocation: true
     }), 'bottom-right');
     //
 
-      //
+    // SEARCH BAR
+            var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+      accessToken: mapboxgl.accessToken,
+      countries: 'br',
+      bbox: [-43.346419534882216,-23.033872959289127,-43.14634834051273,-22.879032876887223], // Set the access token
+      mapboxgl: mapboxgl, // Set the mapbox-gl instance
+      marker: true, // Do not use the default marker style
+    });
+    //
+    // Add the geocoder to the map
+    map.addControl(geocoder);
       // CURRENT POSITION
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 10, maxZoom: 23, duration: 500 });
+    map.fitBounds(bounds, { padding: 10, minZoom: 20,  minZoom: 22, duration: 500 });
     navigator.geolocation.getCurrentPosition(function(position){});
     }
            // Click when page loaded
