4c0d119edd18e1665e4ac62fbe47f864c160151a
breno romeiro <bsromeiro@gmail.com>
2020-03-12 10:57:24 -0300

    Added Done page to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'bathrooms#index'
-  resources :bathrooms, only: [:index, :show, :done, :create]
+  resources :bathrooms, only: [:index, :show, :create] do
+    member do
+      get 'done'
+    end
+  end
   resources :users, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
