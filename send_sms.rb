2dcc8c2d0963693d34beac947670b4df918efab4
Elisa Nogueira <elisanog05@gmail.com>
2020-03-12 15:19:22 -0300

    add Twilio key to env file

--- send_sms.rb
+++ send_sms.rb
@@ -2,7 +2,7 @@
 require 'twilio-ruby'
 
 account_sid = 'AC50429134acd89e32efd6e09da922f93d'
-auth_token = '67f53410b528e5cf8ed3db9f1515d93e'
+auth_token = ENV.auth_token
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
 from = '+18547770150' # Your Twilio number
