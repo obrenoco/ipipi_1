4a36e49d9efe367762cb6b2634c1b81262181ba8
yago santos <yagolcs@msn.com>
2020-03-12 20:03:13 -0300

    Only vote the rating on clicking finish button.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,28 +7,47 @@ import { adminMap } from '../plugins/init_admin_mapbox.js.erb';
 initMapbox();
 adminMap();
 
+let ratingVote = 2.5;
 const bathroomElement = document.getElementById("bathroom-id");
 if (bathroomElement) {
   document.querySelectorAll(".fa-star").forEach((element) => {
     element.addEventListener("click", (event) => {
-      Rails.ajax({
-        url: `/bathrooms/${bathroomElement.dataset.id}/add_rating`,
-        type: "POST",
-        data: `rating=${element.dataset.id}`,
-        success: function(data) {
-          document.querySelectorAll(".fa-star").forEach((starElement) => {
-            if (starElement.dataset.id <= element.dataset.id) {
-              starElement.classList.remove("far");
-              starElement.classList.add("fas");
-
-            } else  {
-              starElement.classList.remove("fas");
-              starElement.classList.add("far");
-            }
-          });
+      ratingVote = element.dataset.id;
+      document.querySelectorAll(".fa-star").forEach((starElement) => {
+        if (starElement.dataset.id <= element.dataset.id) {
+          starElement.classList.remove("far");
+          starElement.classList.add("fas");
+        } else {
+          starElement.classList.remove("fas");
+          starElement.classList.add("far");
         }
       });
+      console.log(ratingVote);
     });
   });
 }
 
+if (bathroomElement) {
+  document.getElementById("finish").addEventListener("click", (event) => {
+    Rails.ajax({
+      url: `/bathrooms/${bathroomElement.dataset.id}/add_rating`,
+      type: "POST",
+      data: `rating=${ratingVote}`,
+      success: function(data) {
+        console.log(data.rating);
+      }
+    });
+  });
+}
+// if (bathroomElement) {
+//   document.getElementById("finish") =>
+//     document.querySelectorAll(".fa-star").forEach((element) => {
+//       element.addEventListener("click", (event) => {
+//         Rails.ajax({
+//           url: `/bathrooms/${bathroomElement.dataset.id}/add_rating`,
+//           type: "POST",
+//           data: `rating=${element.dataset.id}`,
+//         });
+//       });
+//     });
+// }
