41f610a167817f74ef1b4286e5a82fb061a806e1
Elisa Nogueira <elisanog05@gmail.com>
2020-03-13 15:57:33 -0300

    creating new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,17 @@ Bathroom.create!(location: "R. Barão de Icaraí", rating: 1, enabled: true, lat
 
 Bathroom.create!(location: "R. Corrêa Dutra", rating: 5, enabled: true, latitude: -22.926973, longitude: -43.178753 )
 
-Bathroom.create!(location: "Av. Nossa Sra. de Copabacana", rating: 5, enabled: true, latitude: -22.968544, longitude: -43.183602)
+Bathroom.create!(location: "Av. Nossa Sra. de Copabacana", rating: 5,enabled: true, latitude: -22.968544, longitude: -43.183602)
+
+Bathroom.create!(location: "R. Tobias do Amaral", rating: 1, enabled: true, latitude: -22.940538, longitude: -43.200519)
+
+# Bathroom.create!(location: "R. Pinheiro Machado", rating: 4, enabled: false, latitude: -22.938942, longitude: -43.183093)
+
+# Bathroom.create!(location: "R. Mena Barreto", rating: 3, enabled: false, latitude: -22.955184, longitude: -43.188565)
+
+# Bathroom.create!(location: "R. Leblon", rating: 4, enabled: false, latitude: -22.985653, longitude: -43.218053)
+
+#Did not manage to display correctly new seeds, so set them to enabled: false.
 
 User.create(email: "ipipi.contato@gmail.com", password: "capivara", password_confirmation: "capivara")
 
